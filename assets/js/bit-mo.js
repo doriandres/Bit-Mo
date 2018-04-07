1d69e3c2e26e7a1162a57525b91f080984f51d0f
kevav27 <38163635+kevav27@users.noreply.github.com>
2018-04-07 14:38:17 -0600

    including btn events

--- assets/js/bit-mo.js
+++ assets/js/bit-mo.js
@@ -1,1 +1,46 @@
 // Put all the needed code for the app here
+var getElm = function(id){
+	return document.getElementById(id);
+};
+
+var addClick = function(elm, fn){
+	elm.addEventListener("click", fn);
+};
+
+var screen = getElm("screen");
+var addOneToScreen = function(){
+	screen.value += "1";
+}
+var addZeroToScreen = function(){
+	screen.value += "0";	
+}
+var addPlusToScreen = function(){
+	screen.value += "+";
+}
+var addLessToScreen = function(){
+	screen.value += "-";
+}
+var addDivideToScreen = function(){
+	screen.value += "/";
+}
+var addMultiplyToScreen = function(){
+	screen.value += "*";
+}
+
+
+var initBtnEvents = (function(){
+	var oneBtn = getElm("oneBtn");
+	var zeroBtn = getElm("zeroBtn");
+	var plusBtn = getElm("plusBtn");
+	var lessBtn = getElm("lessBtn");
+	var divideBtn = getElm("divideBtn");
+	var multiplyBtn = getElm("multiplyBtn");
+
+
+	addClick(oneBtn, addOneToScreen);
+	addClick(zeroBtn, addZeroToScreen);
+	addClick(plusBtn, addPlusToScreen);
+	addClick(lessBtn, addLessToScreen);
+	addClick(divideBtn, addDivideToScreen);
+	addClick(multiplyBtn, addMultiplyToScreen);
+})();
